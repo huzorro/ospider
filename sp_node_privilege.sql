074ee496a2c7eecc98c2355ed15ab2780aab88a2
huzorro <huzorro@gmail.com>
2016-06-20 10:06:48 +0800

    更新数据结构

--- sp_node_privilege.sql
+++ sp_node_privilege.sql
@@ -1,9 +1,14 @@
-id,name,node,logtime
-4,api添加,/floodapi/add,"2016-06-20 00:55:03"
-4,api编辑,/floodapi/edit,"2016-06-20 00:55:03"
-4,获取单个api,/floodapi/one,"2016-06-20 00:55:03"
-4,api列表,/floodapiview,"2016-06-20 01:07:48"
-4,目标添加,/floodtarget/add,"2016-06-20 05:18:14"
-4,目标编辑,/floodtarget/edit,"2016-06-20 05:18:14"
-4,获取单个目标,/floodtarget/one,"2016-06-20 05:18:14"
-4,目标列表,/floodtargetview,"2016-06-20 05:18:14"
+/*
+-- Query: SELECT * FROM ospider.sp_node_privilege where logtime > "2016-06"
+LIMIT 0, 100
+
+-- Date: 2016-06-20 10:06
+*/
+INSERT INTO `sp_node_privilege` (`id`,`name`,`node`,`logtime`) VALUES (4,'api添加','/floodapi/add','2016-06-20 00:55:03');
+INSERT INTO `sp_node_privilege` (`id`,`name`,`node`,`logtime`) VALUES (4,'api编辑','/floodapi/edit','2016-06-20 00:55:03');
+INSERT INTO `sp_node_privilege` (`id`,`name`,`node`,`logtime`) VALUES (4,'获取单个api','/floodapi/one','2016-06-20 00:55:03');
+INSERT INTO `sp_node_privilege` (`id`,`name`,`node`,`logtime`) VALUES (4,'api列表','/floodapiview','2016-06-20 01:07:48');
+INSERT INTO `sp_node_privilege` (`id`,`name`,`node`,`logtime`) VALUES (4,'目标添加','/floodtarget/add','2016-06-20 05:18:14');
+INSERT INTO `sp_node_privilege` (`id`,`name`,`node`,`logtime`) VALUES (4,'目标编辑','/floodtarget/edit','2016-06-20 05:18:14');
+INSERT INTO `sp_node_privilege` (`id`,`name`,`node`,`logtime`) VALUES (4,'获取单个目标','/floodtarget/one','2016-06-20 05:18:14');
+INSERT INTO `sp_node_privilege` (`id`,`name`,`node`,`logtime`) VALUES (4,'目标列表','/floodtargetview','2016-06-20 05:18:14');

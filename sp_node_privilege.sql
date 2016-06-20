83b39ea42016a995e9c6cead55410737181a664e
huzorro <huzorro@gmail.com>
2016-06-20 09:55:57 +0800

    更新数据结构

---
+++ sp_node_privilege.sql
@@ -0,0 +1,9 @@
+id,name,node,logtime
+4,api添加,/floodapi/add,"2016-06-20 00:55:03"
+4,api编辑,/floodapi/edit,"2016-06-20 00:55:03"
+4,获取单个api,/floodapi/one,"2016-06-20 00:55:03"
+4,api列表,/floodapiview,"2016-06-20 01:07:48"
+4,目标添加,/floodtarget/add,"2016-06-20 05:18:14"
+4,目标编辑,/floodtarget/edit,"2016-06-20 05:18:14"
+4,获取单个目标,/floodtarget/one,"2016-06-20 05:18:14"
+4,目标列表,/floodtargetview,"2016-06-20 05:18:14"

857c09c5666e5202a9bc5a44505d0c5ad79182a8
huzorro <huzorro@gmail.com>
2016-06-20 10:21:58 +0800

    更新数据结构

---
+++ menu.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM ospider.sp_menu_template
+LIMIT 0, 100
+
+-- Date: 2016-06-20 10:20
+*/
+
+INSERT INTO `sp_menu_template` (`id`,`title`,`name`,`logtime`) VALUES (2,'API管理','floodapiview','2016-06-20 05:13:19');
+INSERT INTO `sp_menu_template` (`id`,`title`,`name`,`logtime`) VALUES (2,'目标管理','floodtargetview','2016-06-20 05:13:19');

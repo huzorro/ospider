ed21b5084c5d294ae0555e7aaec21b44689af3b8
huzorro <huzorro@gmail.com>
2016-07-13 22:59:17 +0800

    添加删除功能

--- web/handler/spider_flood_target.go
+++ web/handler/spider_flood_target.go
@@ -258,6 +258,7 @@ func GetfloodTargets(r *http.Request, w http.ResponseWriter, db *sql.DB, log *lo
 
 	switch spUser.Access.Rule {
 	case user.GROUP_PRI_ALL:
+        con = "where a.status in(0, 1) "
 	case user.GROUP_PRI_ALLOW:
 		con = "WHERE a.uid IN(" + strings.Join(spUser.Access.Group, ",") + ") and a.status in(0, 1) "
 	case user.GROUP_PRI_BAN:
